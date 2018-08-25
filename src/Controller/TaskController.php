dae88879afbbba2e1496cb585e91b9672f50a3ed
Simon Rasmussen <simon.d.rasmussen@gmail.com>
2018-08-25 15:50:50 +0200

    added migration, saveTaskTest saves to the database

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -4,17 +4,37 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Task;
 class TaskController extends AbstractController
 {
     /**
      * @Route("/task", name="task")
      */
-    public function index()
-    {
+    public function index() {
 		
         return $this->render('task/index.html.twig', [
             'controller_name' => 'TaskController',
         ]);
+	}
+	/**
+     * @Route("/task/save", name="save")
+     */
+	public function saveTask() {
+		$entityManager =$this->getDoctrine()->getManager();
+		$task = new Task();
+		$task->setName('test');
+		$task->setStatus(false);
+		$task->setUserId(0);
+
+		$entityManager->persist($task);
+		
+        $entityManager->flush();
+
+		
+
+        return new Response('Saved new task with id '.$task->getId() . ' and task name:' . $task->getName());
+
 	}
 	//✅
 }
--- src/Entity/Task.php
+++ src/Entity/Task.php
@@ -17,13 +17,35 @@ class Task
     private $id;
 
     /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
      * @ORM\Column(type="boolean")
      */
     private $status;
 
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $userId;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
     }
 
     public function getStatus(): ?bool
@@ -34,6 +56,18 @@ class Task
     public function setStatus(bool $status): self
     {
         $this->status = $status;
+
+        return $this;
+    }
+
+    public function getUserId(): ?int
+    {
+        return $this->userId;
+    }
+
+    public function setUserId(int $userId): self
+    {
+        $this->userId = $userId;
 
         return $this;
     }
